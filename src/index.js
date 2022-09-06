a2d12d074d487bedee83d55abea3d08ea0d4e4b9
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-09-06 12:28:44 +0600

    beautify code

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import {GoogleOAuthProvider} from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// 96384931447-ni484afarfecl7b3bvpdjd1iuh4hhqbd.apps.googleusercontent.com
-root.render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId="96384931447-ni484afarfecl7b3bvpdjd1iuh4hhqbd.apps.googleusercontent.com">    <App /></GoogleOAuthProvider>;
 
-  </React.StrictMode>
+root.render(
+    <React.StrictMode>
+        <GoogleOAuthProvider clientId="YOURCLIENT_ID">
+            <App/></GoogleOAuthProvider>;
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
